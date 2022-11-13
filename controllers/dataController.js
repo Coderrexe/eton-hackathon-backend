a941743348cc9a520d35d273fdadf2f10bfce541
Coderrexe <shizimo@gmail.com>
2022-11-13 21:03:36 +0000

    Add backend logic for storing initial temperature data.

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -6,6 +6,18 @@ async function storeInitialData(req, res) {
   const targetTemperature = req.body.targetTemperature;
 
   // Store to database.
+  const user = new User({
+    username: username,
+    roomNumber: roomNumber,
+    targetTemperature: targetTemperature,
+  });
+
+  try {
+    user.save();
+    res.status(200).json("Successfully saved new user");
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 }
 
 async function getInitialData(req, res) {
